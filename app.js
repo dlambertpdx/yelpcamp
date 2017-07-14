be5e6ef50bfb2d58cd795f5aa3a1e73e45ddfa0e
Donna <donnaw@gmail.com>
2017-07-14 22:29:13 +0000

    update dburl

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ var commentRoutes     = require("./routes/comments"),
     campgroundRoutes  = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index")
 
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://donna:fr3dd13Cat@ds157712.mlab.com:57712/yelpcamp_donna");
 
-// mongoose.connect("mongodb://localhost/yelp_camp_v12");
-mongoose.connect("mongodb://donna:fr3dd13Cat@ds157712.mlab.com:57712/yelpcamp_donna");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
